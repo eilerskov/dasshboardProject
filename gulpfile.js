4580922641027b851de1cf741a24a3f9ae111bed
Peter <eilerskov92@gmail.com>
2020-09-04 00:25:30 +0200

    tilføjet watcher

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,16 @@ const sass = require("gulp-sass");
 function styles() {
 
     return(
-        gulp.src("css/*.scss")
+        gulp.src("")
             .pipe(sass())
             .pipe(gulp.dest("css"))
     );
 
 }
 
+function watch(){
+    gulp.watch("css/*.scss", styles);
+}
+
 exports.styles = styles;
+exports.watch = watch;
